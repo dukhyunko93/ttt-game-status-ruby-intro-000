8666668b584cb7994f41d98d66394d36248f949b
Duke Ko <dukhyunko93@gmail.com>
2020-03-13 16:35:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,11 +50,12 @@ end
 
 def winner(board)
   WIN_COMBINATIONS.detect do |win_combination|
-      if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-        return "x"
-        elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-        return "O"
-      else
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+     return "x"
+    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+      return "O"
+    else
+      false
     end
   end
 end
