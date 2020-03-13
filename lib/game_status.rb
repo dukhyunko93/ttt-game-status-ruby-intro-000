d1e5f01ecad1eae26c61d6a1876dd8e9800f1a47
Duke Ko <dukhyunko93@gmail.com>
2020-03-13 16:35:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,7 +51,7 @@ end
 def winner(board)
   WIN_COMBINATIONS.detect do |win_combination|
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-     return "x"
+     return "X"
     elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
       return "O"
     else
