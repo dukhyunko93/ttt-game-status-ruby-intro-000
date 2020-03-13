1d6e07ab0de601683e9da91e630e722ad238f9f1
Duke Ko <dukhyunko93@gmail.com>
2020-03-13 16:32:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,10 +20,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   winner = []
   WIN_COMBINATIONS.detect do |win_combination|
-    if board.all? {|x| x == " "}
-      return false
-    elsif (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
-      winner == win_combination
+    if (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+      win_combination == winner
     end
   end
   winner
